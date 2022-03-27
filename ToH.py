901c2aece01a3ddb8667dbbf6e13e6b4b71e500e
Kam <58607149+spyderkam@users.noreply.github.com>
2022-03-26 20:13:05 -0700

    Update ToH.py

--- ToH.py
+++ ToH.py
@@ -23,24 +23,24 @@ while True:
                 and (p3 != [] and disk != p3[0]):
                 break
 
-    def condition(input, pile1, pile2, pile3):
-        if pile1 and input == pile1[0]:
-            del pile1[0]
-        elif pile2 and input == pile2[0]:
-            del pile2[0]
-        elif pile3 and input == pile3[0]:
-            del pile3[0]
+    def condition(input):
+        if p2 and input == p1[0]:
+            del p1[0]
+        elif p2 and input == p2[0]:
+            del p2[0]
+        elif p3 and input == p3[0]:
+            del p3[0]
 
     print("Which pile would you like to stack this disk on?")
     pile = input()
     if pile == 'p1' and (p1 == [] or disk < p1[0]):
-        condition(disk, p1, p2, p3)
+        condition(disk)
         p1.insert(0, disk)
     elif pile == 'p2' and (p2 == [] or disk < p2[0]):
-        condition(disk, p1, p2, p3)
+        condition(disk)
         p2.insert(0, disk)
     elif pile == 'p3' and (p3 == [] or disk < p3[0]):
-        condition(disk, p1, p2, p3)
+        condition(disk)
         p3.insert(0, disk)
     else:
         print("You can't do that!")
