666a959551af1dd4ee600147accd8d5c51856830
Kam <58607149+spyderkam@users.noreply.github.com>
2022-03-26 19:51:45 -0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
+#!user/bin/env ruby
+
 #Keep three arrays, which represents the piles of discs. Pick a representation of the discs to store in the arrays; maybe just a number representing their size.
-
 #In a loop, prompt the user (using gets) and ask what pile to select a disc from, and where to put it.
-
 #After each move, check to see if they have succeeded in moving all the discs, to the final pile. If so, they win
 
 winning_pile = [1, 2, 3, 4]
